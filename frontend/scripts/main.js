2c537d4763f35639172b0e4e0d5f09e27a8ac805
Sajid Mahmud <134144226+sajiiid09@users.noreply.github.com>
2025-11-23 21:34:10 +0600

    Delete frontend directory

--- frontend/scripts/main.js
+++
@@ -1,8 +0,0 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelector('.nav-links');
-
-if (navToggle && navLinks) {
-  navToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('open');
-  });
-}
